809f6bc6daab19420597f7528816b87f615dcf3d
bwambale03 <bwambaleedwin03@gmail.com>
2023-07-24 09:44:40 -0700

    defining the FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+""" Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
 from models.amenity import Amenity
