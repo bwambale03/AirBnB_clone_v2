513307838a22bb43b9237a5757cb52e9dcc88a9e
Edwin Bwambale <bwambaleedwin03@gmail.com>
2023-10-16 13:46:16 -0700

    corrected a var spelling error

--- models/state.py
+++ models/state.py
@@ -32,4 +32,4 @@ class State(BaseModel, Base):
             for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
                     city_list.append(city)
-            return city_lisit
+            return city_list
