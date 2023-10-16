b5c9230c9f2eb0213a9a37a6dab9a9f7fb0f3eb7
Edwin Bwambale <bwambaleedwin03@gmail.com>
2023-10-16 13:02:55 -0700

    Refactor exception handling in do_pack() function

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,5 +13,6 @@ def do_pack():
               format(time.strftime("%Y%m%d%H%M%S")))
         return ("versions/web_static_{}.tgz".format(time.
                                                     strftime("%Y%m%d%H%M%S")))
-    except:
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return None
