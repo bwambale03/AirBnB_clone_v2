4f767346a563561b27d000a525a639503f05786d
Edwin Bwambale <bwambaleedwin03@gmail.com>
2023-10-17 21:45:46 -0700

    removed unused import 'from models import *'

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,7 +5,6 @@ start Flask application
 """
 
 from flask import Flask, render_template
-from models import *
 from models import storage
 app = Flask(__name__)
 
@@ -21,6 +20,7 @@ def states_list():
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
