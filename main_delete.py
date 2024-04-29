93991201160cccd0630ddbc0976bb9256998a524
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-04-29 17:12:14 -0400

    updated the file main_delete.py to include manual filtering of the objects after retrieval

--- main_delete.py
+++ main_delete.py
@@ -6,11 +6,16 @@ from models.state import State
 
 fs = FileStorage()
 
-# All States
-all_states = fs.all(State)
-print("All States: {}".format(len(all_states.keys())))
-for state_key in all_states.keys():
-    print(all_states[state_key])
+# Function to filter states
+def get_all_states():
+    all_objects = fs.all()
+    return {k: v for k, v in all_objects.items() if isinstance(v, State)}
+
+# All States initially
+all_states = get_all_states()
+print("All States: {}".format(len(all_states)))
+for state in all_states.values():
+    print(state)
 
 # Create a new State
 new_state = State()
@@ -19,11 +24,11 @@ fs.new(new_state)
 fs.save()
 print("New State: {}".format(new_state))
 
-# All States
-all_states = fs.all(State)
-print("All States: {}".format(len(all_states.keys())))
-for state_key in all_states.keys():
-    print(all_states[state_key])
+# All States after adding California
+all_states = get_all_states()
+print("All States: {}".format(len(all_states)))
+for state in all_states.values():
+    print(state)
 
 # Create another State
 another_state = State()
@@ -32,17 +37,19 @@ fs.new(another_state)
 fs.save()
 print("Another State: {}".format(another_state))
 
-# All States
-all_states = fs.all(State)
-print("All States: {}".format(len(all_states.keys())))
-for state_key in all_states.keys():
-    print(all_states[state_key])
+# All States after adding Nevada
+all_states = get_all_states()
+print("All States: {}".format(len(all_states)))
+for state in all_states.values():
+    print(state)
 
 # Delete the new State
 fs.delete(new_state)
+fs.save()
 
-# All States
-all_states = fs.all(State)
-print("All States: {}".format(len(all_states.keys())))
-for state_key in all_states.keys():
-    print(all_states[state_key])
+# All States after deletion
+all_states = get_all_states()
+print("All States: {}".format(len(all_states)))
+for state in all_states.values():
+    print(state)
+
