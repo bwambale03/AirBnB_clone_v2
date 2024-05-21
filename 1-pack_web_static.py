1816c904683d26f3099439730d5f168221afbbd7
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-05-21 14:04:50 +0300

    a Bash script that sets up your web servers for the deployment of web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """This generates a .tgz archive from the contents of the web_static folder.
 """
-from fabric.api import local
+from fabric import task
 import time
 
-
-def do_pack():
-    """This generates a tgz archive from web_static folder"""
+@task
+def do_pack(c):
+    """this script generates a tg archive from web_static folder"""
+    print("Packing web_static to .tgz archive")
     try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
+        c.run("mkdir -p versions")
+        timestamp = time.strftime("%Y%m%d%H%M%S")
+        c.run("tar -cvzf versions/web_static_{}.tgz web_static".format(
+            time.strftime("%Y%m%d%H%M%S")))
+        
+        return f"versions/web_static_{timestamp}.tgz"
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f'An error occurred: {e}')
         return None
