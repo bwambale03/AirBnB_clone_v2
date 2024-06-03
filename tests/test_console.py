d457f3563fa92ff48bec0065999df61959c9565e
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-06-03 06:15:20 -0700

    installing unmmet dependencies i.e sqlalchemy, py test modules

--- tests/test_console.py
+++ tests/test_console.py
@@ -157,7 +157,7 @@ class TestConsole(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             self.consol.onecmd("all User")
             obj = f.getvalue()
-        my_id = obj[obj.find('(')+1:obj.find(')')]
+        my_id = obj[obj.find('(') + 1:obj.find(')')]
         with patch('sys.stdout', new=StringIO()) as f:
             self.consol.onecmd("update User " + my_id)
             self.assertEqual(
@@ -222,7 +222,7 @@ class TestConsole(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             self.consol.onecmd("all User")
             obj = f.getvalue()
-        my_id = obj[obj.find('(')+1:obj.find(')')]
+        my_id = obj[obj.find('(') + 1:obj.find(')')]
         with patch('sys.stdout', new=StringIO()) as f:
             self.consol.onecmd("User.update(" + my_id + ")")
             self.assertEqual(
